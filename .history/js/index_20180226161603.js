208ced7c2361d93b823b5ad7948f806cd1c0d708
谢建伟 <xiejianwei@gemdale.com>
2018-02-26 16:58:29 +0800

    github调试配置页

--- .history/js/index_20180226161603.js
+++ .history/js/index_20180226161603.js
@@ -1,7 +1,7 @@
 //先判断是否有网络，如果有网络，就先清理调所有缓存的资源，重新缓存
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('sw.js',{scope:'/flybird.github.io/'}).then(function(registration) {
+      navigator.serviceWorker.register('sw.js',{scope:'/pwa/'}).then(function(registration) {
         // Registration was successful
         const version = '0.0.5';
         const currrent_version = localStorage.getItem('sw_version');
